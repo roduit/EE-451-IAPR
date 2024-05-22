df860901fc4a288438a679df5216e4fc3256b4a4
Vincent Roduit <vincent.roduit@epfl.ch>
2024-05-23 00:15:49 +0200

    added efficient net

---
+++ project/src/models/efficient_net.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+# -*- author : Vincent Roduit-*-
+# -*- date : 2024-05-23 -*-
+# -*- Last revision: 2024-05-23 (Vincent Roduit)-*-
+# -*- python version : 3.9.18 -*-
+# -*- Description: Functions for efficient net -*-
+
+# Import libraries
+import torch
+from torch import nn
+from torch.optim import lr_scheduler
+from torchvision import models
+
+
+
+def predict(model, dataloader):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    model.eval()
+    predictions = []
+
+    for inputs, _ in dataloader:
+        inputs = inputs.to(device)
+        outputs = model(inputs)
+        _, preds = torch.max(outputs, 1)
+        predictions.extend(preds.cpu().numpy())
+
+    return predictions
+
+def train_model(model, dataloaders, criterion, optimizer, scheduler, dataset_sizes, num_epochs=25):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    
+    for epoch in range(num_epochs):
+        print(f'Epoch {epoch}/{num_epochs - 1}')
+        print('-' * 10)
+
+        for phase in ['train', 'val']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0
+
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+
+                optimizer.zero_grad()
+
+                with torch.set_grad_enabled(phase == 'train'):
+                    outputs = model(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    loss = criterion(outputs, labels)
+
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+            if phase == 'train':
+                scheduler.step()
+
+            epoch_loss = running_loss / dataset_sizes[phase]
+            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+        print()
+
+    return model
